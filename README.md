648d51829757a02522c53e025f00ba2d845ac201
chenmengangzhi29 <935325906@qq.com>
2022-05-17 22:15:19 +0800

    v0

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # douyin
+
+1.文件说明
+main.go                     加载路由，初始化路由，将路由器连接到 http.Server并开始侦听和服务 HTTP 请求
+routers                     存放路由相关配置
+controller                  存放具体实现代码
+models                      存放数据库配置，共享结构和工具
+public                      存放本地视频文件
+
+model/example.sql           自动创建数据库
+$ mysql –u用户名 –p密码 –D数据库<【sql脚本文件路径全名】
+
+model/app.ini               通过修改相关信息，自动打开数据库
--- models/example.sql
+++ models/example.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE IF NOT EXISTS `douyin` 
+CREATE DATABASE IF NOT EXISTS `douyin`; 
+USE `douyin`;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
 (
@@ -24,14 +25,14 @@ CREATE TABLE `video`
     `title`       varchar(128)        NOT NULL DEFAULT '' COMMENT '标题',
     `play_url`    varchar(128)        NOT NULL DEFAULT '' COMMENT '播放地址',
     `cover_url`   varchar(128)        NOT NULL DEFAULT '' COMMENT '封面地址',
-    'favorite_count' bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '点赞总数',
-    'comment_count'  bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '评论总数',
-    'create_date'    timestamp         NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `favorite_count` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '点赞总数',
+    `comment_count`  bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '评论总数',
+    `create_date`    timestamp         NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='视频表';
 
-INSERT INTO 'video'
+INSERT INTO `video`
 VALUES (1, 1, 'bear', 'https://www.w3schools.com/html/movie.mp4', 'https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg', 3, 2, CURRENT_TIMESTAMP);
 
 DROP TABLE IF EXISTS `favorite`;
@@ -44,7 +45,7 @@ CREATE TABLE `favorite`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='点赞表';
 
-INSERT INTO 'favorite'
+INSERT INTO `favorite`
 VALUES (1, 1, 1),
        (2, 2, 1),
        (3, 3, 1);
@@ -55,13 +56,13 @@ CREATE TABLE `comment`
     `id`          bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '评论id',
     `user_id`     bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '用户id',
     `video_id`    bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '视频id',
-    'contents'    text                NOT NULL DEFAULT '' COMMENT '评论内容',
-    'create_date' timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `contents`    text                NOT NULL COMMENT '评论内容',
+    `create_date` timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='评论表';
 
-INSERT INTO 'comment'
+INSERT INTO `comment`
 VALUES (1, 1, 1, '这视频也太模糊了！', CURRENT_TIMESTAMP),
        (2, 2, 1, '老CCTV了～', CURRENT_TIMESTAMP);
 
@@ -73,12 +74,12 @@ CREATE TABLE `relation`
     `id`          bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '关注id',
     `user_id`     bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '用户id',
     `to_user_id`  bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '对方用户id',
-    'status'      tinyint             NOT NULL DEFAULT -1 COMMENT '关注状态',
+    `status`      tinyint             NOT NULL DEFAULT -1 COMMENT '关注状态',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='关注表';
 
-INSERT INTO 'relation'
+INSERT INTO `relation`
 VALUES (1, 2, 3, -1);
 
 
