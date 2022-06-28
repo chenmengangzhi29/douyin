b25ffc9d0ed273ec74406494ff173c147c800484
chenmengangzhi29 <935325906@qq.com>
2022-06-28 22:20:51 +0800

    修改日志初始化函数,定制日志输出

--- main.go
+++ main.go
@@ -9,26 +9,24 @@ import (
 
 func main() {
 	if err := Init(); err != nil {
-		util.Logger.Error("init fail")
 		os.Exit(-1)
 	}
+	defer util.Logger.Sync()
 	// 加载多个APP的路由配置
 	routers.Include(routers.Video, routers.User, routers.Favorite, routers.Comment, routers.Follow)
 	// 初始化路由
 	r := routers.Init()
-	// r.Use(gin.Logger())
 	if err := r.Run(); err != nil {
 		return
 	}
 }
 
 func Init() error {
-	if err := model.Init(); err != nil {
-		util.Logger.Error("model fail")
+	if err := util.InitLogger(); err != nil {
 		return err
 	}
-	if err := util.InitLogger(); err != nil {
-		util.Logger.Error("util fail")
+	if err := model.Init(); err != nil {
+		util.Logger.Error("model fail")
 		return err
 	}
 	return nil
